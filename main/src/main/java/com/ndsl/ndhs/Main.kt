acf779970014d1f602555dc9d65daedb838065c2
Bun133 <bun133.work@gmail.com>
2021-05-28 08:30:28 +0900

    まぁよし!

--- main/src/main/java/com/ndsl/ndhs/Main.kt
+++ main/src/main/java/com/ndsl/ndhs/Main.kt
@@ -1,10 +1,12 @@
 package com.ndsl.ndhs
 
 import com.github.bun133.nngraphics.display.JFrameDisplay
+import com.github.bun133.nngraphics.display.Pos
 import com.github.bun133.nngraphics.display.Rect
 import com.ndsl.ndhs.javacv.Buffer
 import com.ndsl.ndhs.javacv.FFmpegFrameGrabber
-import com.ndsl.ndhs.javacv.FrameDrawable
+import com.ndsl.ndhs.javacv.Player
+import com.ndsl.ndhs.javacv.PlayerSeekBar
 import java.io.File
 
 fun main() {
@@ -16,11 +18,11 @@ class BufferTest {
     fun test() {
         val buffer = Buffer<Int>()
         for (i in 0..10) buffer.add(i)
-        var t:Int? = null
+        var t: Int? = null
         do {
             t = buffer.getAndRemove(0)
             println(t)
-        }while (t != null)
+        } while (t != null)
     }
 }
 
@@ -29,9 +31,12 @@ class Main {
     val grabber = FFmpegFrameGrabber(File("main\\src\\main\\resources\\video1.mp4"))
     fun main() {
         grabber.getFrameGrabber().start()
-        d.scene().newLayer().add(FrameDrawable(grabber))
+        val player = PlayerSeekBar(Player(grabber))
+        d.scene().newLayer().add(player)
+        d.mouse.listeners.add(player)
         while (true) {
             d.draw.update()
+//            player.setFrame(100)
         }
     }
 }
--- main/src/main/java/com/ndsl/ndhs/javacv/Player.kt
+++ main/src/main/java/com/ndsl/ndhs/javacv/Player.kt
@@ -1,14 +1,14 @@
 package com.ndsl.ndhs.javacv
 
-import com.github.bun133.nngraphics.display.GraphicsDrawable
-import org.bytedeco.javacv.Frame
+import com.github.bun133.nngraphics.display.*
 import org.bytedeco.javacv.Java2DFrameConverter
+import java.awt.Color
 import java.awt.Graphics
 import java.awt.image.BufferedImage
 import kotlin.concurrent.thread
 import kotlin.math.roundToLong
 
-class FrameDrawable(val grabber: FrameGrabber) : GraphicsDrawable() {
+class Player(val grabber: FrameGrabber) : GraphicsDrawable() {
     var img: BufferedImage? = null
     private val converter = Java2DFrameConverter()
     private var buf: SizedBuffer<BufferedImage> = SizedBuffer(20)
@@ -17,9 +17,17 @@ class FrameDrawable(val grabber: FrameGrabber) : GraphicsDrawable() {
 
     init {
         thread {
-            while (isPlaying) {
-                if (!buf.isFilled()) {
-                    buf.add(converter.getBufferedImage(grabber.grabImage()))
+            while (true) {
+                if (isPlaying && grabber.index <= grabber.getFrameGrabber().lengthInFrames) {
+                    if (!buf.isFilled()) {
+                        try {
+                            val i = converter.getBufferedImage(grabber.grabImage())
+                            if (i != null) buf.add(i)
+                        } catch (e: Exception) {
+                            // 握りつぶします。
+                            println("握りつぶしました")
+                        }
+                    }
                 }
                 Thread.sleep(1)
             }
@@ -27,9 +35,9 @@ class FrameDrawable(val grabber: FrameGrabber) : GraphicsDrawable() {
     }
 
     override fun onDraw(gg: Graphics) {
-        if(!timer.isStarted()) timer.start()
+        if (!timer.isStarted()) timer.start()
         if (img != null) {
-            gg.drawImage(img, 0, 0, null)
+            gg.drawImage(img, p.x, p.y, null)
         }
 
         if (timer.isUp()) {
@@ -40,9 +48,102 @@ class FrameDrawable(val grabber: FrameGrabber) : GraphicsDrawable() {
         }
     }
 
+    var p = Pos(0, 0)
+    override fun pos(): Pos = p
+    override fun setPos(pos: Pos) {
+        p = pos
+    }
+
+    fun setFrame(i: Int) {
+        pause()
+        buf.clear()
+        grabber.set(i)
+        start()
+    }
+
+    fun pause() {
+        isPlaying = false
+    }
+
+    fun start() {
+        isPlaying = true
+    }
+
+    fun getFrame(): Int {
+        return grabber.getFrameGrabber().frameNumber
+    }
+
     private fun getNs(): Long {
         return ((1000L * 1000L * 1000L) / grabber.getFrameGrabber().frameRate).roundToLong()
     }
+
+    fun width() = grabber.getFrameGrabber().imageWidth
+    fun height() = grabber.getFrameGrabber().imageHeight
+    fun size(): Rect = Rect(0, 0, width(), height())
+}
+
+class PlayerSeekBar(val player: Player, var nonFilled: Color = Color.GRAY, var filled: Color = Color.WHITE) :
+    GraphicsDrawable(), MouseBoundedListener {
+    override fun onDraw(gg: Graphics) {
+        player.onDraw(gg)
+
+        drawSeekBar(gg)
+    }
+
+    override fun pos(): Pos {
+        return player.pos()
+    }
+
+    override fun setPos(pos: Pos) {
+        player.setPos(pos)
+    }
+
+    var seekBarHeight = 10
+    var seekBarBottom = 40
+
+    private fun drawSeekBar(gg: Graphics) {
+        gg.color = nonFilled
+        gg.fillRect(
+            player.pos().x,
+            (player.pos().y + player.height()) - (seekBarBottom + seekBarHeight),
+            player.width(),
+            seekBarHeight
+        )
+        gg.color = filled
+        gg.fillRect(
+            player.pos().x,
+            (player.pos().y + player.height()) - (seekBarBottom + seekBarHeight),
+            getFilledWidth(),
+            seekBarHeight
+        )
+    }
+
+    private fun getFilledWidth(): Int {
+        return player.width() * player.getFrame() / player.grabber.getFrameGrabber().lengthInFrames
+    }
+
+    fun seekBarRect() = Rect(
+        player.pos().x,
+        (player.pos().y + player.height()) - (seekBarBottom + seekBarHeight),
+        player.pos().x + player.width(),
+        (player.pos().y + player.height()) - (seekBarBottom + seekBarHeight) + seekBarHeight
+    )
+
+    override fun bound(): Rect = Rect(
+        player.pos().x,
+        (player.pos().y + player.height()) - (seekBarBottom + seekBarHeight),
+        player.pos().x + player.width(),
+        (player.pos().y + player.height()) - (seekBarBottom + seekBarHeight) + (seekBarHeight + seekBarBottom)
+    )
+
+    override fun on(p: Pos, t: Mouse.Type) {
+        if (seekBarRect().contain(p)) {
+            // SeekBar操作
+            player.setFrame(player.grabber.getFrameGrabber().lengthInFrames * (p.x - player.pos().x) / player.width())
+        }
+    }
+
+    override fun type(): List<Mouse.Type> = listOf(Mouse.Type.LeftClick, Mouse.Type.Drag)
 }
 
 /*
@@ -128,6 +229,10 @@ class Buffer<T> {
     }
 
     fun isEmpty() = list.isEmpty()
+
+    fun clear() {
+        list.clear()
+    }
 }
 
 class SizedBuffer<T>(var size: Int) {
@@ -143,28 +248,32 @@ class SizedBuffer<T>(var size: Int) {
     fun isFilled() = (size <= buf.list.size)
 
     fun count() = buf.list.size
+
+    fun clear() {
+        buf.clear()
+    }
 }
 
 class Timer(var nanos: Long) {
     var startTime: Long? = null
-    fun start() : Timer{
+    fun start(): Timer {
         startTime = System.nanoTime()
         return this
     }
 
     fun isUp(): Boolean {
-        if(startTime==null) return false
+        if (startTime == null) return false
         return (System.nanoTime() - startTime!!) >= nanos
     }
 
     fun isStarted() = startTime != null
 
     // 実際には過ぎた時間を考慮するStart
-    fun restart(){
-        if(isStarted()){
-            if(isUp()){
+    fun restart() {
+        if (isStarted()) {
+            if (isUp()) {
                 startTime = startTime!! + nanos
-            }else{
+            } else {
                 start()
             }
         }
